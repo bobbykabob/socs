4875177698b9728a3c8554c65fdd5cf3a91de907
harris <songharris2006@gmail.com>
2022-09-11 21:01:59 -0700

    added comments

--- net_test.py
+++ net_test.py
@@ -58,13 +58,13 @@ for n in range(len(spline_x_position)):
     line.append((spline_x_position[n], spline_y_position[n]))
 
 # shapely buffer code
-
 line = LineString(line)
 dilated = line.buffer(0.001, resolution=1, cap_style=1, join_style=2)
 
 # print vertices
 patch_vertices_x, patch_vertices_y = dilated.exterior.coords.xy
 
+# generate a point cloud
 vertices_3d = []
 
 for n in range(int(len(patch_vertices_x))):
@@ -73,27 +73,34 @@ for n in range(int(len(patch_vertices_x))):
     x_pos = patch_vertices_x[current_index]
     y_pos = patch_vertices_y[current_index]
 
+    # bottom dimension of net
     z_pos = 0.004
     row = [x_pos, y_pos, z_pos, 8, 8, 8]
     vertices_3d.append(row)
+
+    # top dimension of net
     z_pos = 0.008
     row = [x_pos, y_pos, z_pos, 8, 8, 8]
     vertices_3d.append(row)
 
+# input point cloud into Open3D file types
 vertices_3d = numpy.array(vertices_3d)
 print(vertices_3d)
 pcd = o3d.geometry.PointCloud()
+
+# now we include details about the Point Cloud: we need the xyz position, color, normal vectors of each point
 pcd.points = o3d.utility.Vector3dVector(vertices_3d[:, :3])
 pcd.colors = o3d.utility.Vector3dVector(vertices_3d[:, 3:6] / 255)
+
+# we utilize open3d's estimate normal method to estimate each individual point's normal plane
 o3d.geometry.PointCloud.estimate_normals(
     pcd,
     search_param=o3d.geometry.KDTreeSearchParamHybrid(radius=0.1,
                                                       max_nn=30))
-if pcd.has_normals():
-    print("has normal")
 
-# pcd.normals = o3d.utility.Vector3dVector(vertices_3d[:, 6:9])
-# o3d.visualization.draw_geometries([pcd])
+# to create a mesh, we use the Ball Pivoting Algorithm, or bpa, mesh-creating method
+# this blog had a very good explanation:
+# https://towardsdatascience.com/5-step-guide-to-generate-3d-meshes-from-point-clouds-with-python-36bad397d8ba
 
 distances = pcd.compute_nearest_neighbor_distance()
 avg_dist = np.mean(distances)
@@ -101,6 +108,8 @@ radius = 3 * avg_dist
 
 bpa_mesh = o3d.geometry.TriangleMesh.create_from_point_cloud_ball_pivoting(pcd, o3d.utility.DoubleVector(
     [radius, radius * 2]))
+
+# we now post-process our mesh
 dec_mesh = bpa_mesh
 
 dec_mesh.remove_degenerate_triangles()
@@ -108,22 +117,26 @@ dec_mesh.remove_duplicated_triangles()
 dec_mesh.remove_duplicated_vertices()
 dec_mesh.remove_non_manifold_edges()
 
+# we save our mesh into a stl for debugging purposes
 output_path = 'C:/Users/harris/Desktop/CoppeliaSim/python/'
 o3d.io.write_triangle_mesh(output_path + "p_mesh_c.stl", bpa_mesh)
 
+# we now import the net and generate the net into coppeliasim
 vertices, indices = sim.importMesh(0, '/Users/harris/Desktop/CoppeliaSim/python/p_mesh_c.stl', 1, 0, 1)
 
 shading_angle = 20.0 * 3.1415 / 180.0
 
 for i in range(len(vertices)):
     net_handle = sim.createMeshShape(0, shading_angle, vertices[i], indices[i])
-    net_handle_2 = sim.createMeshShape(0, shading_angle, vertices[i], indices[i])
-sim.setObjectPosition(net_handle, sim.handle_world, [0, 0, 0.03])
-sim.setObjectPosition(net_handle_2, sim.handle_world, [0, 0, 0.08])
+    # net_handle_2 = sim.createMeshShape(0, shading_angle, vertices[i], indices[i])
+sim.setObjectPosition(net_handle, sim.handle_world, [0, 0, 0.05])
+# sim.setObjectPosition(net_handle_2, sim.handle_world, [0, 0, 0.08])
 
+# we need to set the object to be respondable to other objects (like the sphere)
+sim.setObjectInt32Param(net_handle, 3004, 1)
 
-sim.setObjectInt32Param(net_handle,3004,1)
-sim.setObjectInt32Param(net_handle_2,3004,1)
+# sim.setObjectInt32Param(net_handle_2,3004,1)
+
 while (t := sim.getSimulationTime()) < 500:
     s = f'Simulation time: {t:.2f} [s]'
     current_time = int(round(time.time() * 1000))
