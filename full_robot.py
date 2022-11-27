e8b98d318148f7ed8692a9218b600a288cae2c1d
harris <songharris2006@gmail.com>
2022-11-27 01:25:48 -0800

    seperating robot functions to its own class, current bug with matplotlib & graphs displaying same info

---
+++ full_robot.py
@@ -0,0 +1,83 @@
+import matplotlib.pyplot as plt
+import numpy
+import cv2
+import matplotlib.pyplot as plt
+
+
+class full_robot:
+    vision_sensors = []
+
+    def __init__(self, sim, name):
+        self.name = name
+        self.y = None
+        self.x = None
+        self.full_img = None
+        self.sim = sim
+        self.handle = self.sim.getObject(name)
+        self.script_handle = self.sim.getScript(1, self.handle)
+        self.sim.initScript(self.script_handle)
+        for i in range(4):
+            a_vision_sensor = self.sim.getObject(name + '/Vision_sensor[' + str(i) + ']')
+            self.vision_sensors.append(a_vision_sensor)
+
+    def generate_full_img(self):
+        self.full_img = []
+        for i in range(len(self.vision_sensors)):
+            self.sim.handleVisionSensor(self.vision_sensors[i])
+            img = self.sim.getVisionSensorDepth(self.vision_sensors[i], self.sim.handleflag_depthbuffer, [0, 0],
+                                                [0, 0])
+
+            img = img[0]
+
+            img = self.sim.unpackFloatTable(img, 0, 65536, 0)
+            img = numpy.array(img).reshape(1, 256, 1)
+
+            img = cv2.flip(img, 0)
+            self.full_img.append(img)
+
+        circular_img = numpy.concatenate((self.full_img[2], self.full_img[3], self.full_img[1], self.full_img[0]),
+                                         axis=1)
+        self.full_img = circular_img
+
+        return self.full_img
+
+    def calculate_polar_coordinates(self):
+        self.x = []
+        self.y = []
+        offset = -3 * numpy.pi / 4
+        data_points = 4 * 256
+
+        for i in range(128):
+            distance = self.full_img[0, i * 8]
+            self.x.append(offset + 2 * numpy.pi * (data_points - i * 8) / data_points)
+            self.y.append(distance)
+
+        return self.x, self.y
+
+    def init_local_graph(self):
+
+        self.x = []
+        self.y = []
+        self.figure, self.ax = plt.subplots(figsize=(10, 10), subplot_kw={'projection': 'polar'})
+        self.line1, = self.ax.plot(self.x, self.y, 'bo')
+        r = numpy.arange(0, 2, 0.01)
+        theta = numpy.pi / 2 + r * 0
+        self.ax.plot(theta, r, color="green")
+        # setting title
+        plt.title("radial view: " + self.name, fontsize=20)
+
+        # setting x-axis label and y-axis label
+        plt.xlabel("X-axis")
+        plt.ylabel("Y-axis")
+
+    def update_local_graph(self):
+        self.line1.set_xdata(self.x)
+        self.line1.set_ydata(self.y)
+        self.figure.canvas.draw()
+        plt.pause(0.000000000001)
+        self.figure.canvas.flush_events()
+
+    def move_robot(self, target_pos, constants):
+        self.sim.callScriptFunction("update_actuation", self.script_handle, target_pos, constants)
+    def show_depth_view(self):
+        cv2.imshow('depth view: ' + self.name, self.full_img)
--- individual_robot_test.py
+++ individual_robot_test.py
@@ -1,13 +1,12 @@
 import time
-from math import pi
-
-
-import numpy
-import matplotlib.pyplot as plt
-from zmqRemoteApi import RemoteAPIClient
-from constants import ROBOT_C1, ROBOT_C2, ROBOT_TRACK_WIDTH, b
 from math import radians
+
 import cv2
+
+from constants import ROBOT_C1, ROBOT_C2, ROBOT_TRACK_WIDTH, b
+from full_robot import full_robot
+from zmqRemoteApi import RemoteAPIClient
+
 # initial setup for client-sim
 client = RemoteAPIClient()
 sim = client.getObject('sim')
@@ -16,103 +15,32 @@ client.setStepping(True)
 sim.startSimulation()
 
 # empty arrays to cycle through
-robots = []
-vision_sensors = []
 prev_time = int(round(time.time() * 1000))
 
-name = '/robot'
-robots.append(sim.getObject(name))
-script_handle = sim.getScript(1, robots[0])
-sim.initScript(script_handle)
-vision_sensors.append(sim.getObject(name + '/Vision_sensor[0]'))
-vision_sensors.append(sim.getObject(name + '/Vision_sensor[1]'))
-vision_sensors.append(sim.getObject(name + '/Vision_sensor[2]'))
-vision_sensors.append(sim.getObject(name + '/Vision_sensor[3]'))
-
-
-for i in range(len(robots)):
+a_robot = full_robot(sim, '/robot[0]')
+b_robot = full_robot(sim, '/robot[1]')
+for i in range(1):
     target_angle = radians(0)
-    sim.callScriptFunction("update_actuation", script_handle, [-10, 10, target_angle],
-                           [ROBOT_C1, ROBOT_C2, ROBOT_TRACK_WIDTH, b])
+    target_pos = [-10, 10, target_angle]
+    constants = [ROBOT_C1, ROBOT_C2, ROBOT_TRACK_WIDTH, b]
+    a_robot.move_robot(target_pos, constants)
 
-sim.setObjectPosition(robots[0], sim.handle_world, [0, 0, 0.1])
-sim.setObjectOrientation(robots[0], sim.handle_world, [0, 0, pi/2])
-
-
-plt.ion()
-
-x = []
-y = []
-figure, ax = plt.subplots(figsize=(10, 10), subplot_kw={'projection': 'polar'})
-line1, = ax.plot(x, y, 'bo')
-
-
-r = numpy.arange(0, 2, 0.01)
-theta = numpy.pi / 2 + r * 0
-line2, = ax.plot(theta,r, color="green")
-# setting title
-plt.title("radial view", fontsize=20)
-
-# setting x-axis label and y-axis label
-plt.xlabel("X-axis")
-plt.ylabel("Y-axis")
-
+a_robot.init_local_graph()
+b_robot.init_local_graph()
 while (t := sim.getSimulationTime()) < 40:
-    # create an empty area to store the image matrices
-    full_img = []
-
-    for i in range(len(vision_sensors)):
-        # handles the Vision Sensor to update (clearing previous image)
-        sim.handleVisionSensor(vision_sensors[i])
-
-        # gets Vision sensor depth informaton
-        img = sim.getVisionSensorDepth(vision_sensors[i], sim.handleflag_depthbuffer, [0, 0], [0, 0])
-
-        # we select the image matrix of the returned value (img[0] is the matrix, img[1] is the resolution matrix)
-        img = img[0]
-
-        # we convert the int32 into int8 format
-        img = sim.unpackFloatTable(img, 0, 65536, 0)
-
-        # reshape into numpy & opencv format
-        img = numpy.array(img).reshape(1, 256, 1)
-
-        # flip the camera
-        img = cv2.flip(img, 0)
-        # append onto the circular image
-        full_img.append(img)
-
-    # concatenate into one matrix; previously it was an array of matirices
-    circular_img = numpy.concatenate((full_img[2],full_img[3],full_img[1],full_img[0]),axis=1)
-    full_img = circular_img
-
-    # show the result
-    cv2.imshow('depth view', full_img)
+    a_robot.generate_full_img()
+    a_robot.show_depth_view()
+    a_robot.calculate_polar_coordinates()
+    a_robot.update_local_graph()
+    b_robot.generate_full_img()
+    b_robot.show_depth_view()
+    b_robot.calculate_polar_coordinates()
+    b_robot.update_local_graph()
 
     # perform calculations with the acquired data; we create a polar graph from the data
     # x represents the theta value
-    x = []
-    # y represents the radius value
-    y = []
-    max_distance = 1
-    offset = -3*numpy.pi/4
 
-    data_points = 4 * 256
-    for i in range(128):
-        distance = circular_img[0, i*8]
-        x.append(offset + 2 * numpy.pi * (data_points - i*8) / data_points)
-        y.append(distance)
 
-    # updating data values
-    line1.set_xdata(x)
-    line1.set_ydata(y)
-    # drawing updated values
-    figure.canvas.draw()
-
-    # This will run the GUI event
-    # loop until all UI events
-    # currently waiting have been processed
-    figure.canvas.flush_events()
 
     # process the user information - cycle time & simulation times
     s = f'Simulation time: {t:.3f} [s]'
